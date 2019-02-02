067830b28b32442fa3f630832780e36d6edf671e
Dinielle <Dinielle.Nelson@gmail.com>
2019-02-01 19:59:32 -0500

    practicing regular push. mountCount correct

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+import os
+import csv
+
+csvpath = os.path.join("..","..","UCFLM201901DATA2","03-Python","Homework",
+	"Instructions","PyBank","Resources","budget_data.csv")
+
+def PyBank(test):
+
+#The total number of months included in the dataset
+	monthCount = sum(1 for line in test)
+
+	print(monthCount)
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The average of the changes in "Profit/Losses" over the entire period
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in losses (date and amount) over the entire period
+
+
+#print("Financial Analysis")
+#print(---------------------)
+
+with open (csvpath,'r') as csvfile:
+	csvreader = csv.reader(csvpath,delimiter=",")
+	csvheader = next(csvreader)
+
+	PyBank(csvreader)
