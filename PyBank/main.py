d70391e008ad11110875a62e598f0b1850f50fb3
Dinielle <Dinielle.Nelson@gmail.com>
2019-02-02 22:25:45 -0500

    Update file with average of changes in Profit/Loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,29 +1,58 @@
 import os
 import csv
 
-csvpath = os.path.join("..","..","UCFLM201901DATA2","03-Python","Homework",
+csv_path = os.path.join("..","..","UCFLM201901DATA2","03-Python","Homework",
 	"Instructions","PyBank","Resources","budget_data.csv")
 
-def PyBank(test):
+def pybank(test):
 
-#The total number of months included in the dataset
-	monthCount = sum(1 for line in test)
+	month_count = 0
+	net_total = 0
+	net_last = 867884
+	net_list = []
+	net_max = 867884
+	month_max = 1/1/2010
+	net_min = 867884
+	month_min = 1/1/2010
 
-#The net total amount of "Profit/Losses" over the entire period
+	for row in test:
+		#The total number of months included in the dataset
+		#Alternative: month_count = sum(1 for row in test)
+		month_count +=1
+		
+		#The net total amount of "Profit/Losses" over the entire periods
+		net_total += int(row[1])
+		
+		#The average of the changes in "Profit/Losses" over the entire period
+		#net_list.append(row[1]+(row+1)[1])
+		net_cur = int(row[1])
+		net_diff = net_cur-net_last
+		net_last = net_cur
+		net_list.append(net_diff)
+		net_avg = sum(net_list)/(month_count)
 
-#The average of the changes in "Profit/Losses" over the entire period
+		#for row in net_list:
+			#The greatest increase in profits (date and amount) over the entire period
+		#	if int(row[1]) > net_max:
+		#		month_max = row[0]
+		#		net_max = int(row[1])
 
-#The greatest increase in profits (date and amount) over the entire period
-
-#The greatest decrease in losses (date and amount) over the entire period
+			#The greatest decrease in losses (date and amount) over the entire period
+		#	if int(row[1]) < net_min:
+		#		month_min = row[0]
+		#		net_min = int(row[1])
 
 
 	print("Financial Analysis")
 	print("---------------------")
-	print(f'Total Months: {monthCount}')
+	print(f'Total Months: {month_count}')
+	print(f'Net Total: ${net_total}')
+	print(f'Average Change: ${net_avg}')
+#	print(f'Greatest Increase in Profits: {month_max} (${net_max})')
+#	print(f'Greatest Decrease in Profits: {month_min} (${net_min})')
 
-with open (csvpath,'r') as csvfile:
-	csvreader = csv.reader(csvpath,delimiter=",")
-	csvheader = next(csvreader)
+with open (csv_path,'r') as csv_file:
+	csv_reader = csv.reader(csv_file,delimiter=",")
+	csv_header = next(csv_reader)
 
-	PyBank(csvreader)
+	pybank(csv_reader)
