387d0b41df9d89b6b6c43cd1a3d5e6934d92b483
Dinielle <Dinielle.Nelson@gmail.com>
2019-02-03 01:13:40 -0500

    percents and grouped totals added. Winner and reduction remains

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,16 @@ import csv
 csv_path = 'O:/Bootcamp/UCFLM201901DATA2/03-Python/Homework/Instructions/PyPoll/Resources/election_data.csv'
 
 
+
 def pypoll(test):
 	total_votes = 0
+	vote_0 = 0
+	vote_1 = 0
+	vote_2 = 0
+	vote_3 = 0
+	vote_4 = 0
+	vote_5 = 0
+
 	candidate_list = []
 	
 	#The total number of votes cast
@@ -16,21 +24,47 @@ def pypoll(test):
 		if row[2] not in candidate_list:
 			candidate_list.append(row[2])
 	
+		# The total number of votes each candidate won
+		elif row[2]==candidate_list[0]:
+			vote_0 += 1
+		elif row[2]==candidate_list[1]:
+			vote_1 += 1
+		elif row[2]==candidate_list[2]:
+			vote_2 += 1
+		elif row[2]==candidate_list[3]:
+			vote_3 += 1
+		#elif row[2]==candidate_list[4]:
+		#	vote_4 += 1
+		#elif row[2]==candidate_list[5]:
+		#	vote_5 += 1
+
 	# The percentage of votes each candidate won
-
-	# The total number of votes each candidate won
-		for item in candidate_list:
-			print(sum(row[1])
-
+		perc_0 = vote_0/total_votes * 100
+		perc_1 = vote_1/total_votes * 100
+		perc_2 = vote_2/total_votes * 100
+		perc_3 = vote_3/total_votes * 100
+		#perc_0 = vote_0/total_votes * 100
+		#perc_0 = vote_0/total_votes * 100
 
 	# The winner of the election based on popular vote.
+	
 
 
 
 	print("Election Results")
 	print("-------------------")
 	print(f'"Total Votes: "{total_votes}')
+	print("-------------------")
 	print(candidate_list)
+	print(f'{candidate_list[0]}: {perc_0}% ({vote_0})')
+	print(f'{candidate_list[1]}: {perc_1}% ({vote_1})')
+	print(f'{candidate_list[2]}: {perc_2}% ({vote_2})')
+	print(f'{candidate_list[3]}: {perc_3}% ({vote_3})')
+	#print(f'{candidate_list[4]}: {perc_0}% ({vote_0})')
+	#print(f'{candidate_list[5]}: {perc_0}% ({vote_0})')
+	print("-------------------")
+	print("Winner: ")
+
 
 with open (csv_path, 'r') as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=",")
